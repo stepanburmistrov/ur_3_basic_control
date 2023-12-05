f0ad8631727f6d94bf7963ccbb093a831d78b8ed
Stepan Burmistrov <88940525+stepanburmistrov@users.noreply.github.com>
2023-12-05 09:41:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 import cv2
 import numpy as np
 import time
-from robot_control import *
-import keyboard
-import threading
-import os
+from robot_control import *  # Импорт функций управления роботом из другого модуля
+import keyboard  # Библиотека для отслеживания нажатий клавиш
+import threading  # Модуль для работы с потоками
+import os  # Модуль для работы с операционной системой
 
+# Функция, определяющая задачи для робота
 def robot_task():
+    # Перемещение робота в рабочую позицию
     move_robot(*robot_positions['work_position'])
+    # Возвращение робота в исходное положение
     move_robot(*robot_positions['home_position'])
+    # Повторное перемещение робота в рабочую позицию
     move_robot(*robot_positions['work_position'])
+    # Вычисление и перемещение робота в новую координату
     x, y = get_robot_cords((175, 175))
     move_robot(x, y, -0.10, 90)
+    # Возврат робота в рабочую и затем в исходную позицию
     move_robot(*robot_positions['work_position'])
     move_robot(*robot_positions['home_position'])
-    os._exit(0)
+    os._exit(0)  # Завершение процесса
 
+# Функция для проверки нажатия клавиши остановки
 def check_for_stop_key():
     while True:
+        # Проверка, нажата ли клавиша Shift для остановки робота
         if keyboard.is_pressed('shift'):
-            robot.stop()
-            os._exit(0)
+            robot.stop()  # Остановка робота
+            os._exit(0)  # Завершение процесса
             break
-        time.sleep(0.01)
+        time.sleep(0.01)  # Небольшая задержка для снижения нагрузки на CPU
 
-
+# Создание потоков для управления роботом и обработки нажатия клавиш
 robot_thread = threading.Thread(target=robot_task)
 key_thread = threading.Thread(target=check_for_stop_key)
 
+# Запуск потоков
 robot_thread.start()
 key_thread.start()
 
+# Ожидание завершения работы потоков
 robot_thread.join()
 key_thread.join()
-
--- robot_control.py
+++ robot_control.py
@@ -6,42 +6,51 @@ import numpy as np
 import math3d as m3d
 import math
 
-
+# Класс для управления подменным (заглушкой) роботом
 class Nedorobot:
-    def __init__(self,ip):
-        self.ip = ip
-    def getl(self):
-        print('nedorobot_ok')
-    def movel(self,coord,a,b,wait='0'):
-        print('go to', self.ip, coord)
+    def __init__(self, ip):
+        self.ip = ip  # Инициализация IP-адреса робота
 
+    def getl(self):
+        print('Nedorobot_ok')  # Метод для вывода состояния робота-заглушки
+
+    def movel(self, coord, a, b, wait='0'):
+        print('Go to', self.ip, coord)  # Метод для имитации перемещения робота-заглушки
+
+# Класс для управления подменным гриппером
 class Nedogripper:
     def __init__(self, robot_id):
-        print('nedogrip_ok')
-    def gripper_action(self,angle):
-        print('gripper goto', angle)
+        print('Nedogripper_ok')  # Выводит сообщение при создании экземпляра класса
 
+    def gripper_action(self, angle):
+        print('Gripper goto', angle)  # Метод для имитации действия гриппера
+
+# Блок try-except для инициализации реального или подменного робота и гриппера
 try:
-    robot = urx.Robot('192.168.2.175')
-    grip = Robotiq_Two_Finger_Gripper(robot)
+    robot = urx.Robot('192.168.2.175')  # Попытка подключения к реальному роботу
+    grip = Robotiq_Two_Finger_Gripper(robot)  # Инициализация гриппера
 except Exception as e:
-    robot = Nedorobot('left_robot')
-    grip = Nedogripper(robot)
-    print('right robot is Nedorobot', e)
+    robot = Nedorobot('robot')  # Использование робота-заглушки при ошибке
+    grip = Nedogripper(robot)  # Использование гриппера-заглушки при ошибке
+    print('Robot is Nedorobot', e)  # Вывод ошибки
 
-state= robot.getl()
-print(state)
+state = robot.getl()  # Получение состояния робота
+print(state)  # Вывод состояния робота
 
+# Словарь с предопределенными позициями робота
 robot_positions = {'work_position': [-0.300, 0.10, 0.000, 90],
                    'home_position': [-0.140, 0.106, 0.190, 90],
                    }
 
+# Функция для расчета угловых координат гриппера
 def angle_gripper(angle):
+    # Преобразование угла для использования в матрицах поворота
     angle = 90 - angle - 0.01
     roll = 0
     pitch = 3.14
     yaw = np.deg2rad(angle)
 
+    # Создание матриц поворота по трем осям
     yawMatrix = np.matrix([
         [math.cos(yaw), -math.sin(yaw), 0],
         [math.sin(yaw), math.cos(yaw), 0],
@@ -60,8 +69,10 @@ def angle_gripper(angle):
         [0, math.sin(roll), math.cos(roll)]
     ])
 
+    # Комбинирование матриц для получения конечной матрицы поворота
     R = yawMatrix * pitchMatrix * rollMatrix
 
+    # Расчет углов Эйлера из матрицы поворота
     theta = math.acos(((R[0, 0] + R[1, 1] + R[2, 2]) - 1) / 2)
     multi = 1 / (2 * math.sin(theta))
 
@@ -72,6 +83,7 @@ def angle_gripper(angle):
 
     return rx, ry, rz
 
+# Функция для преобразования координат в систему координат робота
 def get_robot_cords(x_y):
     robot_coords = []
     x, y = x_y[0], x_y[1]
@@ -81,6 +93,7 @@ def get_robot_cords(x_y):
     robot_coords.append(y_r / 1000)
     return robot_coords
 
+# Функция для управления движением робота
 def move_robot(x, y, z, angle, w=True):
-    rx, ry, rz = angle_gripper(angle)
-    robot.movel([x, y, z, rx, ry, rz], 0.05, 0.05, wait=w)
+    rx, ry, rz = angle_gripper(angle)  # Получение угловых координат для гриппера
+    robot.movel([x, y, z, rx, ry, rz], 0.05, 0.05, wait=w)  # Команда на перемещение робота
